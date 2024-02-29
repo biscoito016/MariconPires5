ac0c28230d0488e9a9c87bff1e4491095054dfd7
kauan <99217639+biscoito016@users.noreply.github.com>
2024-02-29 15:42:12 -0300

    Add files via upload

---
+++ script/elementor-pro-frontend-js-before.js
@@ -0,0 +1,80 @@
+var ElementorProFrontendConfig = {
+                "ajaxurl": "https:\/\/nikolasferreira.com.br\/wp-admin\/admin-ajax.php",
+                "nonce": "66a600b6cd",
+                "urls": {
+                    "assets": "https:\/\/nikolasferreira.com.br\/wp-content\/plugins\/pro-elements\/assets\/",
+                    "rest": "https:\/\/nikolasferreira.com.br\/wp-json\/"
+                },
+                "shareButtonsNetworks": {
+                    "facebook": {
+                        "title": "Facebook",
+                        "has_counter": true
+                    },
+                    "twitter": {
+                        "title": "Twitter"
+                    },
+                    "linkedin": {
+                        "title": "LinkedIn",
+                        "has_counter": true
+                    },
+                    "pinterest": {
+                        "title": "Pinterest",
+                        "has_counter": true
+                    },
+                    "reddit": {
+                        "title": "Reddit",
+                        "has_counter": true
+                    },
+                    "vk": {
+                        "title": "VK",
+                        "has_counter": true
+                    },
+                    "odnoklassniki": {
+                        "title": "OK",
+                        "has_counter": true
+                    },
+                    "tumblr": {
+                        "title": "Tumblr"
+                    },
+                    "digg": {
+                        "title": "Digg"
+                    },
+                    "skype": {
+                        "title": "Skype"
+                    },
+                    "stumbleupon": {
+                        "title": "StumbleUpon",
+                        "has_counter": true
+                    },
+                    "mix": {
+                        "title": "Mix"
+                    },
+                    "telegram": {
+                        "title": "Telegram"
+                    },
+                    "pocket": {
+                        "title": "Pocket",
+                        "has_counter": true
+                    },
+                    "xing": {
+                        "title": "XING",
+                        "has_counter": true
+                    },
+                    "whatsapp": {
+                        "title": "WhatsApp"
+                    },
+                    "email": {
+                        "title": "Email"
+                    },
+                    "print": {
+                        "title": "Print"
+                    }
+                },
+                "facebook_sdk": {
+                    "lang": "pt_BR",
+                    "app_id": ""
+                },
+                "lottie": {
+                    "defaultAnimationUrl": "https:\/\/nikolasferreira.com.br\/wp-content\/plugins\/pro-elements\/modules\/lottie\/assets\/animations\/default.json"
+                }
+            };
